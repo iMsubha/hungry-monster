93251155765b05084faa540ace8d01040a0464f6
iMsubha <subha.fairuz@gmail.com>
2021-02-08 15:54:49 +0600

    clean code and final edit

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
-function searchButton(){
-    const mealSearch = document.getElementById("meal").value;
+const searchButton= () =>{
+    const searchMealItem = document.getElementById("meal").value;
     document.getElementById("meal").value="";
-    getMealData(mealSearch);
+    getMealData(searchMealItem);
 }
 // fetching meal items and display 
-function getMealData(searchMeal){
+const getMealData = searchMeal => { 
    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s`;
    fetch(url)
    .then(res => res.json())
    .then(data => {
-       const meals =data.meals;
-       const mealsDiv = document.getElementById('meals');
+        const meals =data.meals;
+        const mealsDiv = document.getElementById('meals');
+        let searchMealFirstLetter = searchMeal[0].toLowerCase();
+        
+        meals.forEach(meal => {
+            let mealFullName = meal.strMeal.toLowerCase();
 
-       for (let i = 0; i < meals.length; i++) {
-           let meal = meals[i].strMeal.toLowerCase();
-           let firstLetter = searchMeal[0].toLowerCase();
-
-           if(meal.includes(firstLetter)){
-           const mealName = meals[i].strMeal;
-           const mealImage = meals[i].strMealThumb;
-           const mealDiv = document.createElement('meal-thumbnail')
-           mealsDiv.classList =`container ml-4`
-           const mealsInfo=` 
-                <div onclick="displayMealDetails('${mealName}')">
-                <img id="meal-thumb-img" class="meal-thumb-img" src=${mealImage} alt="food-image">
-                <h3 id="meal-title" class="meal-title mt-2 pt-1">${mealName}</h3>
-                </div>`
-           mealDiv.innerHTML = mealsInfo;
-           mealsDiv.appendChild(mealDiv);
-           }
-       }  
+            // checking each meal name from API 
+            // with search meal's first letter 
+            if(mealFullName.includes(searchMealFirstLetter)){
+            const mealName = meal.strMeal;
+            const mealImage = meal.strMealThumb;
+            //create thumbnail to display
+            const mealDiv = document.createElement('meal-thumbnail')
+            mealsDiv.classList =`container ml-4`
+            const mealsInfo=` 
+                    <div onclick="displayMealDetails('${mealName}')">
+                    <img id="meal-thumb-img" class="meal-thumb-img" src=${mealImage} alt="food-image">
+                    <h3 id="meal-title" class="meal-title mt-2 pt-1">${mealName}</h3>
+                    </div>`
+            mealDiv.innerHTML = mealsInfo;
+            mealsDiv.appendChild(mealDiv);
+        } 
+       });  
    })
 }
 
-//display single meals
-function displayMealDetails(name){
-  const url=  `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
- fetch(url)
- .then(res => res.json())
- .then(data => renderMealInfo(data.meals[0]));
+//fetch single meal 
+const displayMealDetails = name =>{
+    const url=  `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => renderMealInfo(data.meals[0]));
 }
-//create html for single meal item 
+
+//create innerHTML for single meal 
 const renderMealInfo = meal =>{
-    //console.log(meal);
     const mealDetailsDiv = document.getElementById('meal-details')
     mealDetailsDiv.classList="d-flex flex-column justify-content-center align-items-center mb-3"
     mealDetailsDiv.innerHTML=`
